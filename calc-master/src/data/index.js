1a1e49c5340f97ec3c4c29dc54c82a34ab3738f5
Vishal Dubey <hdubey64@gmail.com>
2023-06-02 10:19:33 +0530

    Numaric Button Data And Imort it

---
+++ calc-master/src/data/index.js
@@ -0,0 +1,1 @@
+export { default as NumaricButton } from "./numaricButtons";
---
+++ calc-master/src/data/numaricButtons.jsx
@@ -0,0 +1,53 @@
+export default [
+   { number: 1, value: 1, color: "bg-cyan-500", type: "num" },
+   { number: 2, value: 2, color: "bg-cyan-500", type: "num" },
+   { number: 3, value: 3, color: "bg-cyan-500", type: "num" },
+   { number: 4, value: 4, color: "bg-cyan-500", type: "num" },
+   { number: 5, value: 5, color: "bg-cyan-500", type: "num" },
+   { number: 6, value: 6, color: "bg-cyan-500", type: "num" },
+   { number: 7, value: 7, color: "bg-cyan-500", type: "num" },
+   { number: 8, value: 8, color: "bg-cyan-500", type: "num" },
+   { number: 9, value: 9, color: "bg-cyan-500", type: "num" },
+   {
+      symbol: "+",
+      operation: "Addition",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+   {
+      symbol: "-",
+      operation: "Subtraction",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+   {
+      symbol: "*",
+      operation: "Multiplication",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+   {
+      symbol: "/",
+      operation: "Division",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+   {
+      symbol: "%",
+      operation: "Modulo",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+   {
+      symbol: "^",
+      operation: "Exponentiation",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+   {
+      symbol: "=",
+      operation: "Assignment",
+      color: "bg-amber-500",
+      type: "operator",
+   },
+];
