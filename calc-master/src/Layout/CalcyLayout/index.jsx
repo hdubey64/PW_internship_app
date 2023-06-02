eaa166aa3c2cf80126a1b1f2936cc8f924959c30
Vishal Dubey <hdubey64@gmail.com>
2023-06-02 12:10:39 +0530

    Some Class are Removed

--- calc-master/src/Layout/CalcyLayout/index.jsx
+++ calc-master/src/Layout/CalcyLayout/index.jsx
@@ -3,10 +3,8 @@ import Button from "src/component/Button";
 
 const CalcyLayout = () => {
    return (
-      <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-         <div class="rounded-2xl bg-black py-10 text-center">
-            <Button />
-         </div>
+      <div class="rounded-2xl bg-black h-2/3 w-1/2 py-10 text-center">
+         <Button />
       </div>
    );
 };
