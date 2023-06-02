26496e55d2a116d4217b43a92c3bfc640c707d9f
Vishal Dubey <hdubey64@gmail.com>
2023-06-02 12:09:50 +0530

    Some style are fixed

--- calc-master/src/component/Button/index.jsx
+++ calc-master/src/component/Button/index.jsx
@@ -3,18 +3,17 @@ import { NumaricButton } from "src/data";
 
 const Button = () => {
    return (
-      <div>
-         {NumaricButton.map((e) => {
-            return (
-               <main>
-                  <button
-                     className={`group rounded-xl h-20 w-20 m-1 ${e.color} font-black text-5xl text-white relative overflow-hidden`}
-                  >
-                     <div>{!e.number ? e.symbol : e.number}</div>
-                  </button>
-               </main>
-            );
-         })}
+      <div className=" grid grid-cols-4 ">
+         {NumaricButton.map((e) => (
+            <main className={`order-${e.order}`}>
+               <button
+                  className={`group rounded-xl h-20 w-20
+                  } m-1 ${e.color} font-black text-5xl text-white relative overflow-hidden `}
+               >
+                  {!e.number ? e.symbol : e.number}
+               </button>
+            </main>
+         ))}
       </div>
    );
 };
