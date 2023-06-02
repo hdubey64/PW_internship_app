f4f6d191c54ae14fbe7e7ad13f5f2a015fcf6653
Vishal Dubey <hdubey64@gmail.com>
2023-06-02 10:18:51 +0530

    Button Added

--- calc-master/src/component/Button/index.jsx
+++ calc-master/src/component/Button/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { NumaricButton } from "src/data";
 
 const Button = () => {
    return (
       <div>
-         <main className="main">
-            <button class="group rounded-xl h-20 w-20 bg-cyan-500 font-black text-5xl text-white relative overflow-hidden">
-               1
-               <div class="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl"></div>
-            </button>
-         </main>
+         {NumaricButton.map((e) => {
+            return (
+               <main>
+                  <button
+                     className={`group rounded-xl h-20 w-20 m-1 ${e.color} font-black text-5xl text-white relative overflow-hidden`}
+                  >
+                     <div>{!e.number ? e.symbol : e.number}</div>
+                  </button>
+               </main>
+            );
+         })}
       </div>
    );
 };
