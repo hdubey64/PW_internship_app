70568ba717903b612080daf34dfd4f64dab67cdc
Vishal Dubey <hdubey64@gmail.com>
2023-06-02 10:19:55 +0530

    CalcyMaster Layout created

---
+++ calc-master/src/Layout/CalcyLayout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "src/component/Button";
+
+const CalcyLayout = () => {
+   return (
+      <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+         <div class="rounded-2xl bg-black py-10 text-center">
+            <Button />
+         </div>
+      </div>
+   );
+};
+
+export default CalcyLayout;
