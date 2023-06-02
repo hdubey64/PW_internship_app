3e8c3fb185519dac1a9a0ee0b3cc2fb1e8665fc1
Vishal Dubey <hdubey64@gmail.com>
2023-06-02 12:18:31 +0530

    position Fixed

--- calc-master/src/Layout/Home/index.jsx
+++ calc-master/src/Layout/Home/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = ({ children }) => {
-   return <div className="bg-stone-500 h-screen w-screen">{children}</div>;
+   return (
+      <div
+         className="bg-stone-500 flex Class
+Properties
+align-baseline	vertical-align: baseline;
+align-top	vertical-align: top;
+align-middle justify-center items-center h-screen w-screen"
+      >
+         {children}
+      </div>
+   );
 };
 
 export default Home;
